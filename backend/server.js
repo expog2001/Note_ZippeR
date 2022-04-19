07cab14bbabae90dc61d160531d2f85a065675b9
haris <i.there.hariomsingh@gmail.com>
2022-04-19 12:32:22 +0530

    Intitializzed out be and fe

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  console.log(req.params);
+
+  res.json(note);
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
+  console.log(`Server started on PORT ${PORT}`);
+});
